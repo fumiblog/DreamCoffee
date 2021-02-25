2aa3070ba2bd95a24bd28382fa2ab44ab4b80b4d
矢野文久 <fumiblog@yanobunkyuunoMacBook-Pro.local>
2021-02-25 11:59:31 +0900

    [cart_item] 3-1

--- app/controllers/users/cart_items_controller.rb
+++ app/controllers/users/cart_items_controller.rb
@@ -1,21 +1,39 @@
 class Users::CartItemsController < ApplicationController
   def index
-    @cart_items = CartItem.where(user_id: current_user.id)
-    @cart_item = CateItem.find(params[:id])
+    @cart_items = current_user.cart_items
   end
 
   def create
     # byebug
+    @cart = current_cart
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.user_id = current_user.id
-    @cart_item.save!
+    # byebug
+    @cart_item.save
     redirect_to users_cart_items_path
   end
 
   def update
+    @cart_items = current_user.cart_items
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
     redirect_to users_cart_items_path
+  end
+
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to users_cart_items_path
+  end
+
+  def destroy_all
+    @cart_items = current_user.cart_items
+    @cart_items.destroy_all
+    redirect_to users_cart_items_path
+  end
+
+  def set_cart
+    @cart = current_cart
   end
 
   private
